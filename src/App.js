b3b4b12facc4cd91f1c8b636f950b7fc6184c8b0
Philippe Rathé <prathe@gmail.com>
2018-11-07 17:15:16 -0500

    remove material-ui

--- src/App.js
+++ src/App.js
@@ -6,16 +6,12 @@ import reducer from './reducers/reducer';
 import CurrentFlashardAppBar from './components/containers/CurrentFlashardAppBar';
 import CurrentFlashcardDeck from './components/containers/CurrentFlashcardDeck';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import 'typeface-roboto';
-
 const store = createStore(reducer);
 
 class App extends React.Component {
   render() {
     return (
       <>
-        <CssBaseline />
         <Provider store={store}>
           <>
             <CurrentFlashardAppBar />
--- src/components/presentational/FlashcardList.js
+++ src/components/presentational/FlashcardList.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import Flashcard from './Flashcard'
-import Stepper from "@material-ui/core/Stepper";
 
 const FlashcardList = (props) => {
   return (
-    <Stepper orientation="vertical" nonLinear={true}>
-      {props.flashcards.map((flashcard, index) => (
-        <Flashcard key={index} recto={flashcard[0]} verso={flashcard[1]} />
-      ))}
-    </Stepper>
+    props.flashcards.map((flashcard, index) => (
+      <Flashcard key={index} recto={flashcard[0]} verso={flashcard[1]} />
+    ))
   )
 }
 
