3410c331037efa7a7bb8577a37714895e7de985e
Philippe Rathé <prathe@gmail.com>
2018-11-08 17:22:34 -0500

    add dummy buttons, no style

--- src/components/presentational/Flashcard.js
+++ src/components/presentational/Flashcard.js
@@ -3,6 +3,7 @@ import Step from "@material-ui/core/Step";
 import StepLabel from "@material-ui/core/StepLabel";
 import StepContent from "@material-ui/core/StepContent";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button"
 
 const Flashcard = ({recto, verso, ...otherProps}) => {
   return (
@@ -18,6 +19,21 @@ const Flashcard = ({recto, verso, ...otherProps}) => {
           variant="outlined"
           value={recto}
         />
+        <div>
+          <Button
+            variant="contained"
+            size="small"
+          >
+            Next
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            size="small"
+          >
+            GOT IT
+          </Button>
+        </div>
       </StepContent>
     </Step>
   )  
--- src/components/presentational/FlashcardList.js
+++ src/components/presentational/FlashcardList.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Flashcard from './Flashcard'
 import Stepper from "@material-ui/core/Stepper";
 
 const FlashcardList = (props) => {
+  const [activeStep, setActiveStep] = useState(0)
+
   return (
-    <Stepper orientation="vertical" nonLinear={true}>
+    <Stepper
+      orientation="vertical"
+      nonLinear={true}
+      activeStep={activeStep}
+    >
       {props.flashcards.map((flashcard, index) => (
-        <Flashcard key={index} recto={flashcard[0]} verso={flashcard[1]} />
+        <Flashcard
+          key={index}
+          recto={flashcard[0]}
+          verso={flashcard[1]}
+        />
       ))}
     </Stepper>
   )
