708d67d1ffe7c42bf889e3ceb6d9b6c423109943
Philippe Rathé <prathe@gmail.com>
2018-12-17 22:49:02 -0500

    add .eslitrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": ["standard", "plugin:react/recommended", "plugin:jest/recommended"],
+  "settings": {
+    "react": {
+      "version": "16.7.0-alpha.0", // React version, default to the latest React stable release
+      //"flowVersion": "0.53" // Flow version
+    }
+  }
+};
